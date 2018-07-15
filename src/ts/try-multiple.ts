86c089cd9aaf529841af4294726b1f1a51a77310
blacktemplar <blacktemplar@a1.net>
2018-07-15 07:20:10 +0200

    waits 1 second between retries

--- src/ts/try-multiple.ts
+++ src/ts/try-multiple.ts
@@ -2,9 +2,15 @@ export default function tM(callback: ((a: any) => PromiseLike<any>), retries: nu
     if (retries <= 1) {
         return callback;
     }
-    return (a) => callback(a).then(x => x, () => {console.log("Error in tM"); return tM(callback, retries - 1)(a); });
+    return (a) => callback(a).then(x => x, (e) => {console.log(e); return delay(1000)
+        .then(() => tM(callback, retries - 1)(a));});
 }
 
+function delay(ms: number) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, ms)
+    });
+}
 
 export function tMS(callback: ((a: any) => any), retries: number = 5): (a: any) => any {
     if (retries == 1) {
